43907b2ef4af96e6a38975a71e44d6da489b1286
astrosam21 <astrosam.soni@gmail.com>
2021-05-02 05:23:13 +0530

    Updates

---
+++ precache-manifest.31bbb12716666199ba764e85a517e6dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed78238b892a646ccf0249e73136f137",
+    "url": "/medbaazar/index.html"
+  },
+  {
+    "revision": "5f8d00b68c181e326805",
+    "url": "/medbaazar/static/css/2.4b3dd924.chunk.css"
+  },
+  {
+    "revision": "a6a8abebb74beb8ccea9",
+    "url": "/medbaazar/static/css/main.ff1ff965.chunk.css"
+  },
+  {
+    "revision": "5f8d00b68c181e326805",
+    "url": "/medbaazar/static/js/2.cf69e7e6.chunk.js"
+  },
+  {
+    "revision": "804f70cc7a46d1cb46bd6f96af2cd2c4",
+    "url": "/medbaazar/static/js/2.cf69e7e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a8abebb74beb8ccea9",
+    "url": "/medbaazar/static/js/main.3cda1038.chunk.js"
+  },
+  {
+    "revision": "5545676c1bb405ab85fb",
+    "url": "/medbaazar/static/js/runtime-main.251d0af5.js"
+  },
+  {
+    "revision": "e8a957d2906bba4507a59a5f664b3196",
+    "url": "/medbaazar/static/media/Health.e8a957d2.png"
+  },
+  {
+    "revision": "5e9cea1efd173456771971efd39a9437",
+    "url": "/medbaazar/static/media/Wellness1.5e9cea1e.png"
+  },
+  {
+    "revision": "7a6bc10028a79f78e424a9c71499fa77",
+    "url": "/medbaazar/static/media/avatar.7a6bc100.jpg"
+  },
+  {
+    "revision": "c85d977e29d22fc52a9c5a46e790dbe2",
+    "url": "/medbaazar/static/media/flickimg1.c85d977e.jpg"
+  },
+  {
+    "revision": "910077d51f16cb0b3194dd48285219c8",
+    "url": "/medbaazar/static/media/flickimg4.910077d5.jpg"
+  }
+]);
