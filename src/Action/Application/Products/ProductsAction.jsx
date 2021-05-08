3309aba04a8cc6f4158209d75ccbe8acf2d5396b
astrosam21 <astrosam.soni@gmail.com>
2021-05-08 16:31:42 +0530

    cmtt

--- src/Action/Application/Products/ProductsAction.jsx
+++ src/Action/Application/Products/ProductsAction.jsx
@@ -1,8 +1,6 @@
 import Axios from "axios";
 import { GET_CART_ITEMS, GET_PRODUCTS } from "./ProductsActionTypes";
-const endPoint = "http://f5c4ebb40727.ngrok.io";
-
-const endPoint111 = "http://f5c4ebb40727.ngrok.io";
+const endPoint = "https://f5c4ebb40727.ngrok.io";
 const endpoint11 = `https://my-json-server.typicode.com/astrosam21/jsonserver/db`;
 export const getProducts = (dispatch) => {
   // var headers = {
